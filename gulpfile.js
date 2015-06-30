b6e4410d6a7bc07e7eaae8460ba53636446d1f24
kenta-ls <kenta@livestyles.tv>
2015-06-30 16:08:49 +0900

    scsslintを追加、scssの文法チェック

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require("gulp"),
-    sass = require("gulp-sass"),
+    //sass = require("gulp-sass"),
     compass = require('gulp-compass'),
     hologram = require('gulp-hologram'),
     autoprefixer = require("gulp-autoprefixer"),
@@ -8,25 +8,47 @@ var gulp = require("gulp"),
     plumber = require("gulp-plumber"),
     ejs = require("gulp-ejs"),
     cssmin = require('gulp-cssmin'),
+    //csslint = require('gulp-csslint'),
     rename = require('gulp-rename'),
     spritesmith = require('gulp.spritesmith'),
     bower = require('main-bower-files'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    csscomb = require('gulp-csscomb'),
+    please = require('gulp-pleeease'),
+    cache = require('gulp-cached'),
+    scsslint = require('gulp-scss-lint');
 
 //ejs
 gulp.task("ejs", function() {
     gulp.src(
         ["app/dev/ejs/**/*.ejs",'!'+"app/dev/ejs/**/_*.ejs"]
     )
+        .pipe(plumber())
         .pipe(ejs())
         .pipe(gulp.dest("app/public"))
         .pipe(browser.reload({stream:true}));
 });
+//HTML文法チェック
+var htmlhint = require("gulp-htmlhint");
 
+gulp.task('html', function() {
+    gulp.src('app/public/*.html')
+        .pipe(plumber())
+        .pipe(htmlhint())
+        .pipe(htmlhint.reporter())
+});
 //compass
 gulp.task('compass', function(){
     gulp.src('app/dev/sass/**/*scss')
-        .pipe(plumber())
+        .pipe(cache('sass'))
+        .pipe(plumber({
+          //エラーがあったときに、デスクトップで通知を出す
+          errorHandler: notify.onError("Error: <%= error %>")
+        }))
+        .pipe(scsslint({
+            'config': 'scsslint.yml',
+        }))
         .pipe(compass({
             comments: true,
             css: 'app/public/css/',
@@ -36,29 +58,29 @@ gulp.task('compass', function(){
         .pipe(browser.reload({stream:true}));
 });
 
-//sass
- gulp.task("sass", function() {
-     gulp.src("app/dev/sass/**/*scss")
-         .pipe(plumber())
-         .pipe(sass({ style: 'expanded'}))
-         .pipe(autoprefixer())
-         .pipe(gulp.dest("app/public/css/guide"))
-         .pipe(browser.reload({stream:true}));
- });
+// CSS　整頓 => 圧縮 => リネームして吐き出し
+gulp.task('css', function() {
+    gulp.src('app/public/css/common.css')
+        .pipe(cache('css'))
+        .pipe(plumber({
+          //エラーがあったときに、デスクトップで通知を出す
+          errorHandler: notify.onError("Error: <%= error %>")
+        }))
+        .pipe(csscomb())
+        .pipe(please({
+            autoprefixer: {browsers: ["last 2 versions", "ie >= 8"]},//ベンダープレフィックス
+            minifier: true //圧縮の有無 true/false
+        }))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('app/public/css/'))
+        .pipe(browser.reload({stream:true}));
+});
 
 //スタイルガイド生成
 gulp.task('hologram', function() {
   var configGlob = 'hologram/hologram_config.yml';
   gulp.src(configGlob)
     .pipe(hologram());
-});
-
-// css-min
-gulp.task('cssmin', function () {
-  gulp.src('app/public/css/common.css')
-  .pipe(cssmin())
-  .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest('app/public/css/cssmin'));
 });
 
 //Sprite
@@ -101,8 +123,8 @@ gulp.task("server", function() {
 
 //watch
 gulp.task("default",['server'],function() {
-    gulp.watch("app/dev/ejs/**/*.ejs",["ejs"]);
+    gulp.watch("app/dev/ejs/**/*.ejs",["ejs","html"]);
     gulp.watch(["app/dev/js/**/*.js",'!'+"app/dev/js/min/**/*.js"],["js"]);
-    gulp.watch("app/dev/sass/**/*.scss",['compass','sass','hologram','cssmin']);
+    gulp.watch("app/dev/sass/**/*.scss",['compass','css','hologram']);
     gulp.watch("app/dev/sass/base/sprite/images/*.png",["sprite"]);
 });
